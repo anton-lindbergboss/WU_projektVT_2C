f9ce046e4a4e2fe017bd08924ee7126fd7482a49
anton-lindbergboss <anton-lindbergboss@elev.ga.ntig.se>
2020-05-13 12:44:09 +0200

    Lade till mindre bildfiler att ha på hemsidan

--- js/javascript.js
+++ js/javascript.js
@@ -5,6 +5,16 @@ function toggleMeny() {
 
 document.querySelector(".hamburger").addEventListener("click", toggleMeny)
 
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  })
+
+  var hamburger = document.querySelector(".hamburger");
+  hamburger.addEventListener("click", function() {
+    hamburger.classList.toggle("is-active");
+  });
+
 /* Notera att den största delen javascript 
 finns i html-filerna med hjälp av taggen <script>.*/
 
